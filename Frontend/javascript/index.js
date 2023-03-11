29ca69b057833229e44505cd2f0a494a8e25bf00
johnofodile <63129718+johnofodile@users.noreply.github.com>
2023-03-11 10:57:00 +0100

    finalised responsiveness of portfolio site on small screens

--- Frontend/javascript/index.js
+++ Frontend/javascript/index.js
@@ -1,5 +1,14 @@
 var tablinks= document.getElementsByClassName("tab-links");
 var tabcontents=document.getElementsByClassName("tab-contents");
+var sidemenu=document.getElementById("sidemenu");
+const msg=document.getElementById("msg");
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyhA41dM8y4pjlmCuZKWQ6lV91r3ihbdXKLPBhB8reqqizOAgxeeRwlkiMsV0WfkPuX/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  
+
 
 
 
@@ -19,3 +28,22 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
 
 }
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent succesfully"
+        setTimeout(function(){
+            msg.innerHTML="";
+        },5000)
+        form.reset();
+    })
+ 
+      .catch(error => console.error('Error!', error.message))
+  })
